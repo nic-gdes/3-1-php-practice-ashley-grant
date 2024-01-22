be665950dfac3a8d95b36323b56d03eaa1366a64
Ashley Grant <agrant2@nic.edu>
2024-01-22 15:33:21 -0800

    3.1 in class

---
+++ contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $page_title = "Contact";
+    include('./components/head.php');
+?>
+
+<body>
+    <h1>Contact Page</h1>
+    <?php include('./components/nav.php'); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- <head> -->
+    <?php $page_title = "Home";
+        include('./components/head.php'); 
+    ?>
+<!-- </head> -->
+
+<body>
+    <h1>Home Page</h1>
+    <?php include('./components/nav.php'); ?>
+</body>
+</html>

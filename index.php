a7de80b8b8822a09e4bc545c4c740f701839584a
Ashley Grant <agrant2@nic.edu>
2024-01-28 16:43:15 -0800

    figuring out grid and descriptions

--- index.php
+++ index.php
@@ -16,15 +16,27 @@
         <?php include('./components/footer.php'); ?>
 
         <div class="grid">
+            <h1>Poster</h1>
             <div class="grid-item">
-                <img src="./assets/img/GDES_222_FALL2023_LABEL_LAVASURGE_ASHLEY_GRANT" alt="
+                <img src="./assets/img/scuttle.jpeg" alt="
                 Lava Surge Logo">
             </div>
+            <p>Scuttle is a seagull (gannet in the live action adaptation) and friend of Ariel. They appear in the film as an expert on human objects with whom Ariel consults about items she salvages, though their identifications consist of nonsense.</p>
+            <h1>Poster</h1>
             <div class="grid-item">
                 <img src="./assets/img/ariel.webp" alt="">
             </div>
+            <p>Ariel's most defining feature is her long, flowing, thick, bright red hair. From the waist down, she has a green horizontal fish-like tail with sea-green scales and sea-foam green fins and a matching V-shaped waist-line resembling fish-lips that reveals her midriff and her belly button.</p>
+            <h1>Poster</h1>
             <div class="grid-item">
                 <img src="./assets/img/sebastian.jpeg" alt="">
+            </div>
+            <p>Horatio Thelonious Ignacious Crustaceous Sebastian is a red Jamaican crab and a servant of King Triton, and also his main musical composer. His main song is "Under the Sea". He is voiced by Samuel E. Wright in the films, the TV series and the Sebastian segments from the Marsupilami series.</p>
+            <h1>Poster</h1>
+            <div class="grid-item">
+                <img src="./assets/img/ursula.webp" alt="
+                Lava Surge Logo">
+            <p>Ursula, also known as The Sea Witch or Vanessa, is the main antagonist of the 2023 musical fantasy film The Little Mermaid, the live-action remake of the 1989 animated feature film of the same name. She is a cecaelia sea witch and the younger sister of King Triton.</p>
             </div>
         </div>
     </main>

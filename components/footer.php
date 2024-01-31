0cf628507b087a65427cc2ad4fa8f0ef3f63f051
Ashley Grant <agrant2@nic.edu>
2024-01-31 14:10:21 -0800

    space

--- components/footer.php
+++ components/footer.php
@@ -1,14 +1,18 @@
-<nav>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="./contact.php">Contact</a></li>
-    </ul>
-</nav>
+<head><link rel="stylesheet" href="./assets/css/style.css"></head>
 
-<footer>
-    &copy; <?php echo date("Y") ?> GDES. All Rights Reserved.
-</footer>
+<div class="footer-style">
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="./contact.php">Contact</a></li>
+        </ul>
+    </nav>
 
+
+    <footer>
+        &copy; <?php echo date("Y") ?> GDES. All Rights Reserved.
+    </footer>
+</div>
 <script></script>
 </body>
 </html>
